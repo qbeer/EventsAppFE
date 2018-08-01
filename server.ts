ac9b195752d722f6c5005613a3a199f98366525c
Alex Olar <alex.olar@edreamsodigeo.com>
2018-08-01 15:47:57 +0200

    app is served, hopefully runs on heroku as well

--- server.ts
+++ server.ts
@@ -1,9 +1,11 @@
 import * as express from 'express';
+import * as path from "path";
 
 const app: express.Application = express();
-const port: string | number = process.env.PORT || 4200;
+const port = process.env.PORT || 4200;
+const DIST_FOLDER = path.join(process.cwd(), 'dist/make-events');
 
-app.use('/', express.static(__dirname + 'dist/make-events'));
+app.get('*', express.static(path.join(DIST_FOLDER)));
 
 app.listen(port, () => {
     console.log(`App is running on port: ${port}`);
