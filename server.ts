2beaf94c518e8d3ae8abfc5278752e468a7e0f71
Alex Olar <alex.olar@edreamsodigeo.com>
2018-08-03 15:58:41 +0200

    helmet failed as well on heroku

--- server.ts
+++ server.ts
@@ -1,15 +1,12 @@
 import * as express from 'express';
 import * as path from 'path';
 import * as compression from 'compression';
-import * as helmet from 'helmet';
 
 const app: express.Application = express();
 const port = process.env.PORT || 4200;
 const DIST_FOLDER = path.join(process.cwd(), 'dist/make-events');
 
 app.use(compression());
-
-app.use(helmet());
 
 app.get('*', express.static(path.join(DIST_FOLDER)));
 
