d80bc539dddcf0a68d7987c6dc974d731d610d21
Alex Olar <alex.olar@edreamsodigeo.com>
2018-08-03 15:55:42 +0200

    removed cors, failed on heroku

--- server.ts
+++ server.ts
@@ -1,7 +1,6 @@
 import * as express from 'express';
 import * as path from 'path';
 import * as compression from 'compression';
-import * as cors from 'cors';
 import * as helmet from 'helmet';
 
 const app: express.Application = express();
@@ -9,8 +8,6 @@ const port = process.env.PORT || 4200;
 const DIST_FOLDER = path.join(process.cwd(), 'dist/make-events');
 
 app.use(compression());
-
-app.use(cors());
 
 app.use(helmet());
 
