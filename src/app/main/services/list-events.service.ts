173f461357320a81976976be3e2fcbb56390d789
Alex Olar <alex.olar@edreamsodigeo.com>
2018-08-06 11:42:06 +0200

    envrionment files are not working properly O.o

--- src/app/main/services/list-events.service.ts
+++ src/app/main/services/list-events.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../../environments/environment.prod';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
